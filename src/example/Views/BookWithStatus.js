037698340db2f47697b98cfa500cdd9fd0ba15ae
Fernando de Franca <fernandodefranca@gmail.com>
2018-02-10 16:28:27 -0200

    Updates example

--- src/example/Views/BookWithStatus.js
+++ src/example/Views/BookWithStatus.js
@@ -9,6 +9,8 @@ class BookWithStatus extends Component {
     return (
       <div className="BookWithStatus">
         <h2>BookWithStatus</h2>
+        <h4>Status: {this.props.params.status}</h4>
+        <h4>ID: {this.props.params.id}</h4>
         <Link to={`/book/awesome/${prevId}`}>Prev Book</Link>
         &nbsp;
         <Link to={`/book/awesome/${nextId}`}>Next Book</Link>
